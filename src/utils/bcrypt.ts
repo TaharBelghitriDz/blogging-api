14aaf2311746ecca52c341c58f252eba856e0e79
TaharBelghitriDz <tahartlm0@gmail.com>
2022-04-13 03:17:46 +0100

    add bcrypt utils

---
+++ src/utils/bcrypt.ts
@@ -0,0 +1,14 @@
+import { compare, hash } from "bcryptjs";
+
+export const comparePassword = (
+  str: string,
+  str2: string,
+  clb: (err: Error | undefined, rslt: boolean) => void
+) => compare(str, str2, clb);
+
+const saltRounds = 8;
+
+export const HashPassword = (
+  str: string,
+  clb: (err: Error | undefined, hash: string) => void
+) => hash(str, saltRounds, clb);
