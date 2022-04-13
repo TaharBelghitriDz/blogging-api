b71e2cf506fa0cbfff65435f1c8467479c77da43
TaharBelghitriDz <tahartlm0@gmail.com>
2022-04-13 03:42:59 +0100

    Update blog.model.ts

--- src/models/blog.model.ts
+++ src/models/blog.model.ts
@@ -17,13 +17,27 @@ blogSchema.pre<blogInterface>("save", function (this: blogInterface, next) {
   next();
 });
 
+type Query = FilterQuery<blogInterface>;
+
 blogSchema.statics.addBlog = (args: blogInterface) => new blogDb(args).save();
 
 // remove the values you don't want to update from the object before pass it as paramss
-blogSchema.statics.edit = (
-  Query: FilterQuery<blogInterface>,
-  args: blogInterface
-) => blogDb.updateOne(Query, { $set: args });
+
+blogSchema.statics.edit = (Query: Query, args: blogInterface) =>
+  blogDb.findOneAndUpdate(Query, { $set: args });
+
+// this to push all data to any array ... one method for all of them
+//push data used to remove data but not in subdocs
+blogSchema.statics.pushData = (Query: Query, data: object) =>
+  blogDb.findOneAndUpdate(Query, {
+    $push: data,
+  });
+
+// remove data from subdocs
+blogSchema.statics.pullData = (Query: Query, data: object) =>
+  blogDb.findOneAndUpdate(Query, {
+    $pull: data,
+  });
 
 export const blogDb = model<blogInterface, blogModelInterface>(
   "blog",
