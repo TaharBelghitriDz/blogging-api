14c480457a45c70a837157d12780008c77e21937
TaharBelghitriDz <tahartlm0@gmail.com>
2022-04-14 00:48:40 +0100

    initial graphql

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import express, { Request } from "express";
+import { graphqlHTTP } from "express-graphql";
+import schema from "./schemas";
+
+var app = express();
+app.use(
+  "/",
+  graphqlHTTP({
+    schema: schema,
+    graphiql: true,
+    context: ({ req }: { req: Request }) => {
+      const token = req.headers.token;
+    },
+  })
+);
+
+app.listen(4000, () => {
+  console.log("server started on port 4000");
+});
