c5378cafc55637d09ecc0a4ce536cfa45e201291
TaharBelghitriDz <tahartlm0@gmail.com>
2022-04-15 23:59:30 +0100

    login

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,47 @@
-import express, { Request } from "express";
-import { graphqlHTTP } from "express-graphql";
-import schema from "./schemas";
 import "./config/db.connection";
+import { join } from "path";
+import { loadSchemaSync } from "@graphql-tools/load";
+import { GraphQLFileLoader } from "@graphql-tools/graphql-file-loader";
+import { addResolversToSchema } from "@graphql-tools/schema";
+import express from "express";
+import { graphqlHTTP } from "express-graphql";
+import { login, signUp } from "./resolvers/user";
 
-var app = express();
+// Load schema from the file
+const schema = loadSchemaSync(
+  join(__dirname, "./schemas/mutation/index.graphql"),
+  {
+    loaders: [new GraphQLFileLoader()],
+  }
+);
+
+// Write some resolvers
+const resolvers = {
+  Mutation: {
+    signUp,
+    login,
+  },
+  Query: {
+    signUp(h: any, args: any) {
+      return "hi";
+    },
+  },
+};
+
+// Add resolvers to the schema
+const schemaWithResolvers = addResolversToSchema({
+  schema,
+  resolvers,
+});
+
+const app = express();
 app.use(
   "/",
   graphqlHTTP({
-    schema: schema,
+    schema: schemaWithResolvers,
     graphiql: true,
-    context: ({ req }: { req: Request }) => {
-      const token = req.headers.token;
-    },
   })
 );
-
 app.listen(4000, () => {
   console.log("server started on port 4000");
 });
--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -1,5 +1,6 @@
 import { addUser } from "../interfaces/user.interface";
 import { userDb } from "../models/user.model";
+import { comparePassword, HashPassword } from "../utils/bcrypt";
 import { validateEmail } from "../utils/validation";
 
 export const signUp = (
@@ -20,5 +21,26 @@ export const signUp = (
     })
     .catch(() => {
       return { err: "err" };
+    });
+};
+
+export const login = (_: any, args: { email: string; password: string }) => {
+  //check email
+  const checkEmail = validateEmail(args.email);
+  if (!checkEmail) return { err: "unvalid email" };
+
+  return userDb
+    .findOne({ email: args.email })
+    .then((user) => {
+      if (user) return { err: "unvalid emai" };
+      comparePassword(args.password, user.password, (err, result) => {
+        if (err) return { err: "somthng went wrong" };
+        else if (!result) return { err: "unvalid password" };
+        else return { token: "token" };
+      });
+    })
+    .catch((err) => {
+      if (err.err) return { err: err.err };
+      else return { err: "somthing went wrong" };
     });
 };
