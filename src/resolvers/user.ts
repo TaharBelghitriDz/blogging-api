dfe47dcee4cdb811acf6d4051978026467987cd0
TaharBelghitriDz <tahartlm0@gmail.com>
2022-04-16 02:05:43 +0100

    Update user.ts

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -1,46 +1,58 @@
 import { addUser } from "../interfaces/user.interface";
 import { userDb } from "../models/user.model";
-import { comparePassword, HashPassword } from "../utils/bcrypt";
-import { validateEmail } from "../utils/validation";
+import { comparePassword } from "../utils/bcrypt";
+import { tokenSign } from "../utils/token";
+import { checkName, validateEmail } from "../utils/validation";
 
 export const signUp = (
   _: any,
   { name, email, password, checkpassword }: addUser & { checkpassword: string }
 ) => {
-  //check email
+  const chceckNames = checkName(name);
+  if (!chceckNames) return { err: " unvalid name" };
+
   const checkEmail = validateEmail(email);
   if (!checkEmail) return { err: "unvalid email" };
 
-  // check password
   if (password !== checkpassword)
     return { err: "unvalid password check again" };
+
   return userDb
-    .addUser({ name, email, password })
-    .then(() => {
-      return { token: "token" };
+    .findOne({ $or: [{ email }, { name }] })
+    .then((user) => {
+      if (user)
+        throw { err: (user.name === name ? "name" : "email") + " is unavlid" };
+      else return;
     })
-    .catch(() => {
-      return { err: "err" };
+    .then(() => userDb.addUser({ name, email, password }))
+    .then((user) => {
+      const token = tokenSign(user._id);
+      if (token) return { token: token };
+      else throw { err: "somthing went wrong #2" };
+    })
+    .catch((err) => {
+      return { err: err.err || "somthing went wrong" };
     });
 };
 
 export const login = (_: any, args: { email: string; password: string }) => {
-  //check email
   const checkEmail = validateEmail(args.email);
   if (!checkEmail) return { err: "unvalid email" };
 
   return userDb
     .findOne({ email: args.email })
     .then((user) => {
-      if (user) return { err: "unvalid emai" };
-      comparePassword(args.password, user.password, (err, result) => {
-        if (err) return { err: "somthng went wrong" };
-        else if (!result) return { err: "unvalid password" };
-        else return { token: "token" };
-      });
+      if (!user) return { err: "unvalid email" };
+      console.log("login ... " + user.password + args.password);
+      return [comparePassword(args.password, user.password), user];
+    })
+    .then((data: any) => {
+      if (!data[0]) throw { err: "unvalid password" };
+      const token = tokenSign(data[1]._id);
+      if (token) return { token: token };
+      else throw { err: "somthing went wrong #2" };
     })
     .catch((err) => {
-      if (err.err) return { err: err.err };
-      else return { err: "somthing went wrong" };
+      return { err: err.err || "somthing went wrong" };
     });
 };
