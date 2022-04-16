fac9c4cb8052b3f91def7342a2233f67d5d4380a
TaharBelghitriDz <tahartlm0@gmail.com>
2022-04-16 18:54:05 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,55 +1,24 @@
-import "./config/db.connection";
-import { join } from "path";
-import { loadSchemaSync } from "@graphql-tools/load";
-import { GraphQLFileLoader } from "@graphql-tools/graphql-file-loader";
-import { addResolversToSchema } from "@graphql-tools/schema";
 import express from "express";
 import { graphqlHTTP } from "express-graphql";
-import { login, signUp } from "./resolvers/user";
 import dotenv from "dotenv";
 dotenv.config({ path: __dirname + "/.env" });
-
-import { userDb } from "./models/user.model";
-
-// Load schema from the file
-const schema = loadSchemaSync(
-  join(__dirname, "./schemas/mutation/index.graphql"),
-  {
-    loaders: [new GraphQLFileLoader()],
-  }
-);
-
-// Write some resolvers
-const resolvers = {
-  Mutation: {
-    signUp,
-    login,
-  },
-  Query: {
-    signUp(h: any, args: any) {
-      return "hi";
-    },
-  },
-};
-
-// Add resolvers to the schema
-const schemaWithResolvers = addResolversToSchema({
-  schema,
-  resolvers,
-});
-
-// userDb.find({ email: "gitnawi0@gmail.com" }, (err) => {
-//   console.log(err);
-// });
+import "./config/db.connection";
+import schemas from "./schemas";
+import { blogDb } from "./models/blog.model";
 
 const app = express();
 app.use(
   "/",
   graphqlHTTP({
-    schema: schemaWithResolvers,
+    schema: schemas,
     graphiql: true,
   })
 );
+
+blogDb.findOneAndRemove({});
+blogDb.findOne({ likes: 0 }, (e: any, r: any) => {
+  console.log(r);
+});
 const PORT = process.env.PORT || 5005;
 app.listen(PORT, () => {
   console.log("server started on port " + PORT);
